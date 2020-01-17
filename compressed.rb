23f2cbace1379310ba9a22e641c6236c9d07d132
ToriK17 <vfcad@protonmail.com>
2020-01-17 09:25:37 -0800

    adds parameters for compressed algorithm

---
+++ compressed.rb
@@ -0,0 +1,5 @@
+# implement a method to preform basic string compression using the counts of repeated characters. 
+# Example: 
+# input: 'aabcccccaaa' returns: 'a2b1c5a3'
+# If the compressed string would not become smaller than the original string then the method should return the original string. 
+# You can assume that the string given will only contain uppercase and lowercase letters (a-z)
