d819e8b6d0d080ddc499de1ee640358d212fe39a
ToriK17 <vfcad@protonmail.com>
2020-01-17 10:52:50 -0800

    adds complete compressed algorithm

--- compressed.rb
+++ compressed.rb
@@ -3,3 +3,21 @@
 # input: 'aabcccccaaa' returns: 'a2b1c5a3'
 # If the compressed string would not become smaller than the original string then the method should return the original string. 
 # You can assume that the string given will only contain uppercase and lowercase letters (a-z)
+
+def compressed(string)
+  strArray = string.split('').sort()
+  compressed = []
+  i = 0
+  while i < strArray.length
+    if strArray[i] != strArray[(i + 1)]
+      num = strArray.count(strArray[i])
+      compressed << strArray[i] 
+      compressed << num 
+    end  
+    i += 1 
+  end  
+  p compressed.join()
+end
+
+compressed('abcdefggg')
+compressed('abcdefgggaabb')
